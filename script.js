669d74d12823470904ac2e336923939fd8cd0d4a
NgooMinhTri <144891647+ngominhtri05@users.noreply.github.com>
2024-02-14 11:05:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,153 @@
-function NoClick() {
-  const button = document.getElementById("yesbutton");
-  const sizeIncrease = 30;
-  const maximumSize = 5000; // Pixels
-  const currentSize = parseInt(button.style.fontSize) || 16;
-  const newSize = Math.min(currentSize + sizeIncrease, maximumSize);
-  button.style.fontSize = newSize + "px";
-  changeText()
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({ checkUpdate: false });
+const fs = require('fs');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
-const phrases = [
-  "No, choose again",
-  "Are you sure?",
-  "pick me pls",
-  "think clearly",
-  "Is that your final answer??",
-  "im very handsome ",
-  "im gonna cry",
-];
-var currentTextIndex = 0;
-function changeText() {
-  var button = document.getElementById("nobutton");
-  button.innerHTML = phrases[currentTextIndex];
-  currentTextIndex = (currentTextIndex + 1) % phrases.length;
+
+client.on('ready', () => {
+    console.log(`Đã đăng nhập với tài khoản ${client.user.tag}!`);
+});
+i = 0;
+async function fuckyou() {
+    const channelID = '1102870817550905384';
+    const channel = client.channels.cache.get(channelID);
+    a = "a";
+    if (channel) {
+        while(true){
+            if (i % 9 === 0) {
+                await sleep((Math.floor(Math.random() * 2)+1)*1000);
+                channel.send('owo');
+                console.log('owo');
+            }
+            if (i % 12 === 0) {
+                await sleep((Math.floor(Math.random() * 2)+1)*1000);
+                channel.send('owoh');
+                await sleep((Math.floor(Math.random() * 2)+1)*1000);
+                channel.send('owob');
+                console.log('hunt/battke');
+            }
+            client.on('message', async (message) => {
+                if (message.channel.id === '1102870817550905384') {
+                    await sleep(1000);
+                    console.log(`channel: ${message.content}`);
+                    await sleep(1000);
+                    a =  message.content;
+                }
+                if (a === 'stop') {
+                        await sleep(1000);
+                        const fs = require('fs');
+
+                        fs.readFile('D:\\all\\songs\\Alan Walker\\c.mp3', 'utf8', (err, data) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        
+                            console.log(data);
+                        });                        
+                        process.exit();
+                    }
+                if (a.includes('captcha')) {
+                        await sleep(1000);
+                        const fs = require('fs');
+
+                        fs.readFile('D:\\all\\songs\\Alan Walker\\c.mp3', 'utf8', (err, data) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        
+                            console.log(data);
+                        });
+                        await sleep(1000);
+                        //process.exit();
+                    }
+                if (a.includes('(1/5)')) {
+                        await sleep(1000);
+                        const fs = require('fs');
+
+                        fs.readFile('D:\\all\\songs\\Alan Walker\\c.mp3', 'utf8', (err, data) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        
+                            console.log(data);
+                        });
+                        await sleep(1000);
+                        //process.exit();
+                    }
+                if (a.includes('(2/5)')) {
+                        await sleep(1000);
+                        const fs = require('fs');
+
+                        fs.readFile('D:\\all\\songs\\Alan Walker\\c.mp3', 'utf8', (err, data) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        
+                            console.log(data);
+                        });         
+                        await sleep(1000);               
+                        //process.exit();
+                    }
+                if (a.includes('(3/5)')) {
+                        await sleep(1000);
+                        const fs = require('fs');
+
+                        fs.readFile('D:\\all\\songs\\Alan Walker\\c.mp3', 'utf8', (err, data) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        
+                            console.log(data);
+                        });            
+                        await sleep(1000);            
+                        //process.exit();
+                    }
+                if (a.includes('(5/5)')) {
+                        await sleep(1000);
+                        const fs = require('fs');
+
+                        fs.readFile('D:\\all\\songs\\Alan Walker\\c.mp3', 'utf8', (err, data) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        
+                            console.log(data);
+                        });        
+                        await sleep(1000);                
+                        //process.exit();
+                    }
+                if (a.includes('(4/5)')) {
+                        await sleep(1000);
+                        const fs = require('fs');
+
+                        fs.readFile('D:\\all\\songs\\Alan Walker\\c.mp3', 'utf8', (err, data) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                        
+                            console.log(data);
+                        });    
+                        await sleep(1000);                    
+                        //process.exit();
+                    }
+                
+                })
+            i++;
+            await sleep(1000);
+        }
+    } else {
+        console.log('con cac');
+        process.exit();
+    }
 }
-function YesClick() {
-  var buttonsContainer = document.querySelector("div > div");
-  buttonsContainer.innerHTML = "";
-  var elementsToRemove = document.getElementsByClassName("text");
-  while (elementsToRemove.length > 0) {
-    elementsToRemove[0].parentNode.removeChild(elementsToRemove[0]);
-  }
-  var container = document.querySelector("div");
-  container.insertAdjacentHTML(
-    "beforeend",
-    "<p style='font-size: 50px;  border: 5px'>YAYYYYYYYYYYYY!!!!!!!!!!</p>"
-  );
-}
+
+client.login("OTk1NjExODU4MTk3Mjk5MjMz.GjxuiI.1jylRzlzdVLJH207DVNmkpWNPrAmcBuVl-W7Qk");
